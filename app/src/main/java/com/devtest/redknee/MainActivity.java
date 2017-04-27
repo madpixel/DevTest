4f1cb425ef73c459fc83d135bc838260da6ff140
Ygor Duarte <ygordlp@gmail.com>
2017-04-27 15:41:24 -0300

    Add TestFairy integration

--- app/src/main/java/com/devtest/redknee/MainActivity.java
+++ app/src/main/java/com/devtest/redknee/MainActivity.java
@@ -3,11 +3,15 @@ package com.devtest.redknee;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.testfairy.TestFairy;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TestFairy.begin(this, "40c6f96d105b5f7b6f19e2232cf5377c67dba927");
     }
 }
